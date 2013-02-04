df4ee136ca950722bc8ec8f0aaf43d9bc246d664
Mihail Fedorov <mihail@fedorov.net>
2013-02-04 05:03:17 +0400

    Some comments, added hop skip as var

--- index.js
+++ index.js
@@ -1,20 +1,32 @@
 var http = require('http');
 var traceroute = require('./traceroute');
 
+// Need a lot of refactoring away from lasagna
+// Will make it public to make myself force doing this...
+
 http.createServer(function (req, res) {
 
 	var query = require('url').parse(req.url);
-	var queryparams = require('querystring').parse(query['query']);
-	var target = queryparams['host'];
-	var ipa = target.match(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/);
 	var response = '';
 
+	if (typeof query['query'] != 'undefined') {
+		var queryparams = require('querystring').parse(query['query']);
 
+		if (typeof queryparams['host'] != undefined) {
+			var target = queryparams['host'];
+			var ipa = target.match(/^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/);
+		} else {
+			var ipa = null;
+		}
+	} else {
+		var ipa = null;
+	}
 
 	if (null != ipa) {
 
 		traceroute.run({
 			host: target,
+			skip: 4,
 			onData: function(data) {
 				response = response + data;
 			},
@@ -29,7 +41,7 @@ http.createServer(function (req, res) {
 		});
 	} else {
 		res.writeHead(200, {'Content-Type': 'text/plain'});
-		res.write('Incorrect IP!');
+		res.write('Hi there!');
 		res.end();
 	}
 
--- traceroute.js
+++ traceroute.js
@@ -1,9 +1,12 @@
+// Original made by 0101
+// https://github.com/0101/traceroute/
+
 (function() {
   var spawn;
   spawn = require('child_process').spawn;
   exports.run = function(kwargs) {
     var traceroute;
-    traceroute = spawn('traceroute', ['-m 25', '-w 3', '-f 4', kwargs.host]);
+    traceroute = spawn('traceroute', ['-m 25', '-w 3', '-f '+kwargs.skip, kwargs.host]);
     traceroute.stdout.on('data', kwargs.onData);
     traceroute.stderr.on('data', kwargs.onError);
     return traceroute.on('exit', kwargs.onExit);
